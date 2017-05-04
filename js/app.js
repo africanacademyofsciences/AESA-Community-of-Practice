d3908d9b992d9fa2b5c63b7ab3be88665d897864
nelsonkimaiga <nelsonkimaiga@aol.com>
2017-05-04 16:05:50 +0300

    h3-africa navabar markup

--- js/app.js
+++ js/app.js
@@ -57,3 +57,22 @@ function checkAnimation() {
 $(window).scroll(function () {
     checkAnimation();
 });
+
+// ********************************************************
+// NAV BAR
+// 
+$(function(){
+
+    // Returns width of browser viewport
+    var browser = $(window).width();
+    // Returns width of HTML document
+    var document = $(document).width();
+
+    $('.nav .toggle-nav').click(function(e){
+        $('.nav .nav-mobile').addClass('style-mobile').slideToggle('slow');
+        e.preventDefault();
+    });
+
+});
+
+
