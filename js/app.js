8e541524a03abb31162f3078a578d2c662076965
nelsonkimaiga <nelsonkimaiga@aol.com>
2017-08-23 16:11:00 +0300

    indent

--- js/app.js
+++ js/app.js
@@ -73,4 +73,20 @@ $(function(){
 
 });
 
+// ********************************************************
+// NAV BAR
+// 
+$(function(){
+
+    // Returns width of browser viewport
+    var browser = $(window).width();
+    // Returns width of HTML document
+    var document = $(document).width();
+
+    $('.nav .toggle-nav').click(function(e){
+        $('.nav .nav-mobile').addClass('style-mobile').slideToggle('slow');
+        e.preventDefault();
+    });
+
+});
 
